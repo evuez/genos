b7182e70da39e570ff612cc27429f526f92ddcb8
evuez <helloevuez@gmail.com>
2016-09-15 18:15:11 +0200

    Fix help commands list

--- apps/worker/lib/broker/help.ex
+++ apps/worker/lib/broker/help.ex
@@ -1,5 +1,5 @@
 defmodule Broker.Help do
   @behaviour Worker.Broker
 
-  def handle(_), do: {:ok, "Available commands are /sc /ex /sc /h"}
+  def handle(_), do: {:ok, "Available commands are /sh /ex /sc /h"}
 end
